5a6dae6423f9d5a3576611700ebcef58afffa63f
Lilian Daminelli Aguirre <lilidaminelli@gmail.com>
2023-04-21 15:43:53 +0200

    update banner img

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,9 @@
-import banner from "../../assets/img/banner.png";
+import banner from "../../assets/img/banner.svg";
 import "./Banner.css";
 
 function Banner() {
   return (
     <header className="banner">
-      {/* <img src="../../../public/img/banner.png" alt="Main Banner Organo" /> */}
       <img src={banner} alt="Main Banner Organo" />
     </header>
   );
